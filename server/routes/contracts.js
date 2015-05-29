dc55b041b6d19fdfbc441ff4f46912e6ddfc364c
clholzin <clholzin@yahoo.com>
2015-05-29 12:47:33 -0700

    added excel routes

--- server/routes/contracts.js
+++ server/routes/contracts.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var exceljs = require('exceljs');
+var Excel = require("exceljs");
+var XLSX = require('xlsx');
+var kexcel = require('kexcel');
+var fs = require('fs');
 var _ = require("underscore");
 var excelReport = require('excel-report');
 var Contract = require('./../models/contract');
@@ -40,6 +43,8 @@ router.post('/contract',function(req, res){
         res.status(200).json(contract);
     });
 });
+
+
 router.get('/contract/export',function(req, res){
     console.log('get contract export by id');
     //Contract.findOne( {"_id": req.params.id}, function(err, contract) {
@@ -65,24 +70,7 @@ router.get('/contract/export',function(req, res){
             }
             res.end();
         });
-        /**var data ={title:'Voucher List',company:'STP software',address:'56, 13C Street, Binh Tri Dong B ward, Binh Tan district, Ho Chi Minh City',user_created:'TRUONGPV'};
-         data.table1 =[{date:new Date(),number:1,description:'description 1',qty:10},{date:new Date(),number:2,description:'description 2',qty:20}]
-         var template_file = './server/templates/template.xlsx';
-         excelReport(template_file,data,function(error,binary){
-            if(error){
-                res.writeHead(400, {'Content-Type': 'text/plain'});
-                res.end(error);
-                console.log(error);
-                 return;
-            }
-          //  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-          //  res.setHeader("Content-Disposition", "attachment; filename=report.xlsx");
-        });
-         // res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-         // res.setHeader("Content-Disposition", "attachment; filename=report.xlsx");
-         //res.end(binary, 'binary');
-         res.end();
-         // res.json(contract);**/
+
     });
 });
 router.get('/contract/:id',function(req, res){
@@ -93,6 +81,157 @@ router.get('/contract/:id',function(req, res){
     });
 });
 
+router.get('/contract/:id/exportV1',function(req, res){
+    var id = req.params.id;
+    console.log('get contract export by id: '+ id);
+    Contract.findOne( {"_id": id}, function(err, contract) {
+        console.log(err);
+        if(err) throw err;
+        var workbook = XLSX.readFile('./server/templates/demo_qcrM_.xlsx', {cellStyles:true});
+       // var worksheetContract = workbook.SheetNames[9];//04_contract
+      //  var worksheet = workbook.Sheets[worksheetContract];
+         /**  var Changetitle = 'J20';//'Con_1_Title';
+        var Changedescript = 'J21';
+        var title_cell = worksheet[Changetitle];
+        var description_cell = worksheet[Changedescript];**/
+
+      /** _.each(worksheetContract,function(k,v){
+          console.log(k + ' : '+ v);
+       });**/
+
+       /** var sheet_name_list = workbook.SheetNames;
+        sheet_name_list.forEach(function(y) {
+            var worksheet = workbook.Sheets[y];
+            for (z in worksheet) {
+              // all keys that do not begin with "!" correspond to cell addresses
+                if(z[0] === '!') continue;
+                console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+            }
+        });**/
+       // console.log(JSON.stringify(worksheet));
+       // console.log(worksheet["!ref"]);
+      //  console.log(worksheet["!merges"]);
+       // console.log(worksheet["!cols"]);
+
+       // console.log(JSON.stringify(title_cell));
+        /* Get the value */
+       // worksheet[Changetitle] = contract.title;
+       // worksheet[Changedescript] = contract.description.body;
+        // var desired_value =  desired_cell.v;
+
+      XLSX.writeFile(workbook, './server/templates/demo_qcrM_out.xlsx',{cellStyles:true,bookSST:true});//{bookSST:true}
+
+    //  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    //    res.setHeader("Content-Disposition", "attachment; filename=./server/templates/demo_qcrM_out.xlsx");
+
+    });
+
+
+   res.end();
+});
+router.get('/contract/:id/exportV2',function(req, res){
+    var id = req.params.id;
+    console.log('get contract export by id: '+ id);
+    Contract.findOne( {"_id": id}, function(err, contract) {
+        console.log(err);
+        if(err) throw err;
+        //./server/templates/demo_qcrM_.xlsx
+        var template = './server/templates/demo_qcrM_.xlsx';
+        var workbook = new Excel.Workbook();
+        workbook.xlsx.readFile(template)
+            .then(function() {
+                var ws = wb.getWorksheet(0);
+                console.log(JSON.stringify(ws));
+
+            });
+
+        /** workbook.xlsx.writeFile(filename)
+         .then(function() {
+                // done
+            });**/
+    });
+    res.end();
+});
+router.get('/contract/:id/exportV3',function(req, res){
+    var id = req.params.id;
+    console.log('get contract export by id: '+ id);
+    res.setHeader('Content-disposition', 'attachment; filename=test.xlsx');
+    res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    Contract.findOne( {"_id": id}, function(err, contract) {
+        console.log(err);
+        if(err) throw err;
+        //./server/templates/demo_qcrM_.xlsx
+
+       var template = './server/templates/demo_qcrM_.xlsx';
+        kexcel.open(template, function(err, workbook) {
+            // Get first sheet
+            var sheet = workbook.getSheet(5);
+            sheet.setCellValue(1,1,'Hello World!');
+            sheet.setRowValues(2, ['Hello', 'even', 'more', 'Worlds']);
+            sheet.setRowValues(3, [1, '+', 2, 'equals','=A3+C3']);
+            // Duplicate a sheet
+            var duplicatedSheet = workbook.duplicateSheet(5,'My duplicated sheet');
+
+            // Save the file
+           // var output = fs.createWriteStream(__dirname + '/tester.xlsx');
+
+            workbook.pipe(res);
+        });
+
+    });
+    //res.end();
+});
+router.get('/contract/:id/exportV4',function(req, res){
+    var id = req.params.id;
+    console.log('get contract export by id: '+ id);
+    res.setHeader('Content-disposition', 'attachment; filename=test.xlsx');
+    res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    Contract.findOne( {"_id": id}, function(err, contract) {
+        console.log(err);
+        if(err) throw err;
+        //./server/templates/demo_qcrM_.xlsx
+
+        //var template = './server/templates/contract_reporting_template.xlsx';
+        var template = './server/templates/single_contract_temp.xlsx';
+
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+        res.setHeader("Content-Disposition", "attachment; filename=contract_reporting.xlsx");
+        var data ={
+            id:id,
+            title:contract.title,
+            value:contract.contract.amount,
+            description:contract.description.body,
+            startDate:moment.utc(contract.startDate).format('DD/MM/YYYY'),
+            endDate:moment.utc(contract.endDate).format('DD/MM/YYYY'),
+            firm:parseFloat(contract.pricing.firmPricing).toFixed(2),
+            fixed:parseFloat(contract.pricing.fixedPricing).toFixed(2),
+            cost:parseFloat(contract.pricing.costPlusPricing).toFixed(2),
+            estimate:parseFloat(contract.pricing.estBasedFee).toFixed(2),
+            volume:parseFloat(contract.pricing.volDrivenPricing).toFixed(2),
+            target:parseFloat(contract.pricing.targetPricing).toFixed(2),
+            user_created:'clholzin'
+        };
+
+       /** data.table1 = [];
+        _.each(contract,function(item,key){
+            data.table1.push(item);
+        });**/
+
+        excelReport(template,data,function(error,binary){
+            if(!error){
+                res.end(binary, 'binary');
+            }
+            if(error){
+                res.writeHead(400, {'Content-Type': 'text/plain'});
+                //  return  res.write(error);
+                console.log(error);
+            }
+            res.end();
+        });
+
+    });
+    //res.end();
+});
 router.put('/contract/:id',function(req, res){
     console.log(req.body);
     console.log('put collection by id to mongo');
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
-var path = require("path");
+/**var path = require("path");
 exports.index = function(req, res){
     res.render('index', { title: "Passport-Examples"});
 };
 exports.ping = function(req, res){
     res.send("pong!", 200);
-};
+};**/
